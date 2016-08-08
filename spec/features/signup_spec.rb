b22d89e26d3b5dc5b2d1cc6fae24269f98edb83f
cyberplanner <saleh.gfx@gmail.com>
2016-08-08 14:28:42 +0100

    Adds feature test for signup.

---
+++ spec/features/signup_spec.rb
@@ -0,0 +1,12 @@
+
+feature "#Signup" do
+  scenario "user can sign up" do
+    visit '/'
+    fill_in :email, with: "example@mail.com"
+    fill_in :password, with: "secret123456"
+    fill_in :password_confirmation, with: "secret123456"
+    click_button "Sign Up"
+    expect(page).to have_content "Welcome, example@mail.com"
+  end
+
+end
