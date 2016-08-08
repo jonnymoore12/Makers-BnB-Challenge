eee7932823edb839df6b7da6990c25136a054141
Jonny Moore <jonnymoore121212@gmail.com>
2016-08-08 14:59:11 +0100

    Sign up test passes. Signed up user sees welcome

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
 require 'sinatra/base'
 
 class BnB < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'super_secret'
+
   get '/' do
-    'Hello BnB!'
+    erb :index
+  end
+
+  post '/users' do
+    session[:email] = params[:email]
+    redirect '/'
   end
 
   # start the server if ruby file executed directly
