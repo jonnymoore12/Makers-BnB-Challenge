54eac0b1313e59adf3ad2657f10fb117d1b2c019
Arunas Skirius <arukomp@gmail.com>
2016-08-11 13:25:11 +0100

    Fixes the error when comparing empty string with a Time object

--- models/space.rb
+++ models/space.rb
@@ -19,6 +19,9 @@ class Space
                         :method => :positive_duration
 
   def positive_duration
+    if @available_to == "" || @available_from == ""
+      return [false, "Space cannot be available for negative duration"]
+    end
     if @available_to > @available_from
       return true
     else
@@ -38,7 +41,7 @@ class Space
 
   def in_the_past?(date)
     if date == ""
-      return true
+      return [false, "Space cannot be available in the past"]
     elsif date.strftime("%d/%m/%Y") >= Time.now.strftime("%d/%m/%Y")
       return true
     else
