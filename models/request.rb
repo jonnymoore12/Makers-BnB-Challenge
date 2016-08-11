783834ee9dcb8e4e5d44575918d6c837bb2fc7c2
cyberplanner <saleh.gfx@gmail.com>
2016-08-11 15:04:26 +0100

    :constrution: WIP user cannot book his own space #28.

--- models/request.rb
+++ models/request.rb
@@ -5,6 +5,17 @@ class Request
   property :status,   String
   property :date,     Time, required: true
 
+  # validates_with_method :guest, method: :is_guest?
+  #
+  # def is_guest?
+  #   if space.user == @guest
+  #     [false, "You can't book your own space"]
+  #   else
+  #     true
+  #   end
+  # end
+
+
   belongs_to :guest, 'User', :key => true
   belongs_to :host,  'User', :key => true
   belongs_to :space, 'Space',:key => true
--- spec/features/sending_requests_spec.rb
+++ spec/features/sending_requests_spec.rb
@@ -1,16 +1,23 @@
 feature "Sending a request" do
 
-  scenario "a user can send a request and get a confirmation" do
+  scenario "User can send a request and get a confirmation" do
     sign_up
     create_space
     send_request
     expect(page).to have_content "Thanks, your booking request is pending!"
   end
 
-  scenario "user must pick a date to send a request" do
+  scenario "User must pick a date to send a request" do
     sign_up
     create_space
     send_request(start_date: "")
     expect(page).to have_content "Please, pick a date!"
   end
+
+  # scenario "User cannot book his own space" do
+  #   sign_up
+  #   create_space
+  #   send_request
+  #   expect(page).to have_content "You can't book your own space"
+  # end
 end
